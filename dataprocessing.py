757b401daba042d1cecfbf1498e8406dda9bd14d
Jakaho <jakob@ahokas.se>
2024-02-13 14:38:23 +0100

    fixed FFT window processing with tagdata

--- dataprocessing.py
+++ dataprocessing.py
@@ -9,6 +9,22 @@ import matplotlib.pyplot as plt
 
 from scipy.signal import butter, filtfilt
 
+def parse_acc_data(row):
+    # Split the string by commas and convert to integers
+    acc_values = np.array(row.split(',')).astype(int)
+    # Reshape the array to (-1, 3) assuming the pattern ax, ay, az continuously
+    reshaped_values = acc_values.reshape(-1, 3)
+    # Return separate lists for ax, ay, az
+    return reshaped_values[:, 0], reshaped_values[:, 1], reshaped_values[:, 2]
+
+def process_new_format(filepath):
+    filepath = '../datasets/Fredrik_funny_walk.csv'
+    data = pd.read_csv(filepath, dtype={'label': str}, na_values='null', low_memory=False)
+    column_names = ['unknown1', 'unknown2', 'tag_id', 'unknown3', 'unknown4', 'lat', 'long', 'timestamp', 'unknown5', 'acc_data']
+    data.columns = column_names
+    data[['ax', 'ay', 'az']] = pd.DataFrame(data['acc_data'].apply(parse_acc_data).tolist(), index=data.index)
+    return data
+
 # Define the high-pass filter
 def butter_highpass(cutoff, fs, order=5):
     nyq = 0.5 * fs  # Nyquist Frequency
@@ -39,41 +55,42 @@ def ac_components(data, mode, cutoff, fs):
     else:
         raise ValueError(f"Invalid mode: {mode}")
 
-def import_and_downsample(filepath, plot, mode, categories):
-    data = pd.read_csv(filepath, dtype={'label': str}, na_values='null', low_memory=False) #change nulls to NaN
-    data = data.dropna(subset=['label']) #Drop NaN rows (undefined / other behavior)
-    # Filter the DataFrame
-    data= data[data['label'].isin(categories)]
+def import_and_downsample(filepath, plot, mode, categories=None, tagdata=False):
+    if tagdata == True:
+        data = process_new_format(filepath)
+        filtered_data = data[['ax', 'ay', 'az']].copy()
+        
+    else:  
+        data = pd.read_csv(filepath, dtype={'label': str}, na_values='null', low_memory=False) #change nulls to NaN
+        data = data.dropna(subset=['label']) #Drop NaN rows (undefined / other behavior)
+        # Filter the DataFrame
+        data= data[data['label'].isin(categories)] 
+        filtered_data = data[['ax', 'ay', 'az', 'label', 'animal_ID', 'timestamp_ms']]
+        data25 = filtered_data.iloc[::4, :].copy() #downsample 100Hz data to 25Hz data
 
+        # Calculate window index for each sample
+        # 10 second windows and conversion from ms to s
+        data25['window_index'] = data25['timestamp_ms'] // 10000
 
-    filtered_data = data[['ax', 'ay', 'az', 'label', 'animal_ID', 'timestamp_ms']]
-    data25 = filtered_data.iloc[::4, :].copy() #downsample 100Hz data to 25Hz data
+        # Group data by window_index, and then split into individual windows
+        windows = [window for _, window in data25.groupby('window_index')]
 
-    # Calculate window index for each sample
-    # 10 second windows and conversion from ms to s
-    data25['window_index'] = data25['timestamp_ms'] // 10000
-
-    # Group data by window_index, and then split into individual windows
-    windows = [window for _, window in data25.groupby('window_index')]
-
-    #data25.to_csv('datasets/filtered_data_25hz.csv', index=False)
-    del data, filtered_data #no longer need the huge data sets, free up memory to make it run faster
+        #data25.to_csv('datasets/filtered_data_25hz.csv', index=False)
+        del data, filtered_data #no longer need the huge data sets, free up memory to make it run faster
     
-    # Determine the number of samples per window (10 seconds * 25 Hz)
-    samples_per_window = 10 * 25
-
-    total_windows = len(data25) // samples_per_window
-
 
     #test plot first window(s)
-    for axis in ['ax', 'ay', 'az']:
-        for window in range(total_windows):
-            if window == 0:
-                start_idx = window * samples_per_window
-                end_idx = start_idx + samples_per_window
-                data_window = data25.iloc[start_idx:end_idx]
-                
-                if plot ==True:
+    if plot ==True:
+        # Determine the number of samples per window (10 seconds * 25 Hz)
+        samples_per_window = 10 * 25
+        total_windows = len(data25) // samples_per_window
+        
+        for axis in ['ax', 'ay', 'az']:
+            for window in range(total_windows):
+                if window == 0:
+                    start_idx = window * samples_per_window
+                    end_idx = start_idx + samples_per_window
+                    data_window = data25.iloc[start_idx:end_idx]
                     plt.figure(figsize=(10, 4))
                     plt.plot(data_window['timestamp_ms'] / 1000, data_window[axis], label=axis)
                     
@@ -84,48 +101,62 @@ def import_and_downsample(filepath, plot, mode, categories):
                     plt.legend()
                     plt.tight_layout()
                     plt.show()
-
-    del data25
-    
-    windows_ac = []
-
+                    
+                    
+                    
     fs = 10.0       # sample rate, Hz
     cutoff = 0.3    # desired cutoff frequency of the filter, Hz
-    #mode = 'HP'     # choose 'HP', 'mean' or 'jerk'
-    min_length = 18
-    for window in windows:
-       if len(window['ax']) >= min_length:
-            window['ac_ax'] = ac_components(window['ax'], mode, cutoff, fs)
-            window['ac_ay'] = ac_components(window['ay'], mode, cutoff, fs)
-            window['ac_az'] = ac_components(window['az'], mode, cutoff, fs)
+    windows_ac = []
+
+    if tagdata == False:
+        #mode = 'HP'     # choose 'HP', 'mean' or 'jerk'
+        min_length = 18
+        if tagdata == False:
+            for window in windows:
+                if len(window['ax']) >= min_length:
+                    window['ac_ax'] = ac_components(window['ax'], mode, cutoff, fs)
+                    window['ac_ay'] = ac_components(window['ay'], mode, cutoff, fs)
+                    window['ac_az'] = ac_components(window['az'], mode, cutoff, fs)
+                    windows_ac.append(window)
             
-            windows_ac.append(window)        
-
-        #plotting both AC variants to see difference
-    if plot ==True:
-        plt.figure(figsize=(15, 6))
-        plt.plot(windows_ac[0]['timestamp_ms'] / 1000, windows_ac[0]['ax'], label='Original ax')
-        plt.plot(windows_ac[0]['timestamp_ms'] / 1000, windows_ac[0]['ac_ax'], label='AC component of ax', linestyle='--')
-        plt.title('Original and AC Component of ax Over Time')
-        plt.xlabel('Time (s)')
-        plt.ylabel('Acceleration (g)')
-        plt.legend()
-        plt.show()
+                    #plotting both AC variants to see difference
+        if plot ==True:
+            plt.figure(figsize=(15, 6))
+            plt.plot(windows_ac[0]['timestamp_ms'] / 1000, windows_ac[0]['ax'], label='Original ax')
+            plt.plot(windows_ac[0]['timestamp_ms'] / 1000, windows_ac[0]['ac_ax'], label='AC component of ax', linestyle='--')
+            plt.title('Original and AC Component of ax Over Time')
+            plt.xlabel('Time (s)')
+            plt.ylabel('Acceleration (g)')
+            plt.legend()
+            plt.show()
 
 
-        plt.figure(figsize=(15, 6))
-        plt.plot(windows_ac[0]['timestamp_ms'] / 1000, windows_ac[0]['ax'], label='Original ax')
-        plt.plot(windows_ac[0]['timestamp_ms'] / 1000, windows_ac[0]['jerk_ax'], label='Jerk component of ax', linestyle='--')
-        plt.title('Original and AC Component of ax Over Time')
-        plt.xlabel('Time (s)')
-        plt.ylabel('Acceleration (g)')
-        plt.legend()
-        plt.show()
-            
+            plt.figure(figsize=(15, 6))
+            plt.plot(windows_ac[0]['timestamp_ms'] / 1000, windows_ac[0]['ax'], label='Original ax')
+            plt.plot(windows_ac[0]['timestamp_ms'] / 1000, windows_ac[0]['jerk_ax'], label='Jerk component of ax', linestyle='--')
+            plt.title('Original and AC Component of ax Over Time')
+            plt.xlabel('Time (s)')
+            plt.ylabel('Acceleration (g)')
+            plt.legend()
+            plt.show()       
+       
+    else:
+        filtered_data['ac_ax'] = filtered_data['ax'].apply(lambda x: ac_components(np.array(x), mode, cutoff, fs))
+        filtered_data['ac_ay'] = filtered_data['ay'].apply(lambda x: ac_components(np.array(x), mode, cutoff, fs))
+        filtered_data['ac_az'] = filtered_data['az'].apply(lambda x: ac_components(np.array(x), mode, cutoff, fs))
+        
+    if tagdata:
+        windows_ac = [filtered_data.iloc[[i]].copy() for i in range(len(filtered_data))]
     return windows_ac
 
-def process_window(windows_ac, windowID=None):
-    sampling_rate = 25  # 25 Hz sampling rate
+
+
+def process_window(windows_ac, windowID=None, tagdata=False):
+    if tagdata:
+        sampling_rate = 6
+    else:
+        sampling_rate = 25  # 25 Hz sampling rate
+    
     window_size = sampling_rate  # 1-second window for 1 Hz resolution
     overlap = window_size // 2  # 50% overlap
     hann_window = get_window('hann', window_size)
@@ -143,10 +174,16 @@ def process_window(windows_ac, windowID=None):
         for ID in window_indices:
             # Initialize frequency bands for the current window and axis
             all_time_domain_signals[axis][ID] = {f: [] for f in range(6)}
-            window_data = windows_ac[ID][f'ac_{axis}'].to_numpy()
             
+            if tagdata:
+                window_data = windows_ac[ID][f'ac_{axis}'].to_numpy()[0]
+            else:
+                window_data = windows_ac[ID][f'ac_{axis}'].to_numpy()
+            #print(window_data)
+            #print(len(window_data)) == 1??? varför
             for i in range(0, len(window_data) - window_size + 1, overlap):
                 window_segment = window_data[i:i+window_size] * hann_window
+                
                 fft_result = fft(window_segment, n=window_size)
                 
                 for f in range(6):
@@ -157,18 +194,18 @@ def process_window(windows_ac, windowID=None):
 
             # Concatenate overlapping windows for each frequency band
             for f in all_time_domain_signals[axis][ID]:
-                concatenated_signal = [all_time_domain_signals[axis][ID][f][0]]
-                for segment in all_time_domain_signals[axis][ID][f][1:]:
-                    concatenated_signal.append(segment[overlap:])
-                all_time_domain_signals[axis][ID][f] = np.concatenate(concatenated_signal)
-            
+                if all_time_domain_signals[axis][ID][f]:  # Check if the list is not empty
+                    concatenated_signal = [all_time_domain_signals[axis][ID][f][0]]
+                    for segment in all_time_domain_signals[axis][ID][f][1:]:
+                        concatenated_signal.append(segment[overlap:])
+                    all_time_domain_signals[axis][ID][f] = np.concatenate(concatenated_signal)
+
             if windowID is not None and axis == 'ax':
                 # Plot only for the X-axis if windowID is provided
                 plot_signals(all_time_domain_signals[axis][windowID], windowID, axis)
     
     # If no specific windowID is provided, all_time_domain_signals contains all axes for all windows
     return all_time_domain_signals
-
 def plot_signals(time_domain_signals, windowID, axis):
     time_vector = np.linspace(0, 10, num=len(time_domain_signals[0]), endpoint=False)
     plt.figure(figsize=(15, 6))
@@ -192,4 +229,5 @@ def plot_signals(time_domain_signals, windowID, axis):
         ax.legend()
     plt.tight_layout()
     plt.show()
+    
     
