b26bb1f0f611b3fc07ff9757d99c65b4271b9368
Jakaho <jakob@ahokas.se>
2024-02-16 15:50:04 +0100

    sampling rate bugfix

--- dataprocessing.py
+++ dataprocessing.py
@@ -111,15 +111,14 @@ def import_and_downsample(filepath, plot, mode, categories=None, tagdata=False):
     if tagdata == False:
         #mode = 'HP'     # choose 'HP', 'mean' or 'jerk'
         min_length = 18
-        if tagdata == False:
-            for window in windows:
-                if len(window['ax']) >= min_length:
-                    window['ac_ax'] = ac_components(window['ax'], mode, cutoff, fs)
-                    window['ac_ay'] = ac_components(window['ay'], mode, cutoff, fs)
-                    window['ac_az'] = ac_components(window['az'], mode, cutoff, fs)
-                    windows_ac.append(window)
-            
-                    #plotting both AC variants to see difference
+        for window in windows:
+            if len(window['ax']) >= min_length:
+                window['ac_ax'] = ac_components(window['ax'], mode, cutoff, fs)
+                window['ac_ay'] = ac_components(window['ay'], mode, cutoff, fs)
+                window['ac_az'] = ac_components(window['az'], mode, cutoff, fs)
+                windows_ac.append(window)
+        
+                #plotting both AC variants to see difference
         if plot ==True:
             plt.figure(figsize=(15, 6))
             plt.plot(windows_ac[0]['timestamp_ms'] / 1000, windows_ac[0]['ax'], label='Original ax')
@@ -166,13 +165,11 @@ def import_and_downsample(filepath, plot, mode, categories=None, tagdata=False):
     return windows_ac
 
 
-
 def process_window(windows_ac, windowID=None, tagdata=False):
     if tagdata:
         sampling_rate = 6
-    else:
+    else: 
         sampling_rate = 25  # 25 Hz sampling rate
-    
     window_size = sampling_rate  # 1-second window for 1 Hz resolution
     overlap = window_size // 2  # 50% overlap
     hann_window = get_window('hann', window_size)
@@ -190,7 +187,11 @@ def process_window(windows_ac, windowID=None, tagdata=False):
         for ID in window_indices:
             # Initialize frequency bands for the current window and axis
             all_time_domain_signals[axis][ID] = {f: [] for f in range(6)}
-            window_data = windows_ac[ID][f'ac_{axis}']
+            
+            if tagdata:
+                window_data = windows_ac[ID][f'ac_{axis}'].to_numpy()[0]
+            else:
+                window_data = windows_ac[ID][f'ac_{axis}'].to_numpy()
             #print(window_data)
             #print(len(window_data)) == 1??? varför
             for i in range(0, len(window_data) - window_size + 1, overlap):
@@ -241,5 +242,3 @@ def plot_signals(time_domain_signals, windowID, axis):
         ax.legend()
     plt.tight_layout()
     plt.show()
-    
-    
